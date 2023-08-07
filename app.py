e190eba7e814970ce9377ab00a8fd680ed39841e
tyler19970426 <139113291+tyler19970426@users.noreply.github.com>
2023-08-07 11:54:01 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -27,11 +27,38 @@ def callback():
 
     return "OK"
 
-#處理訊息
+#處理訊息(回傳同樣的訊息)
 @handler.add(MessageEvent, message=TextMessage)
 def handel_message(event):
-    message = TextSendMessage(text = event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
+  
+    emoji = [
+            {
+                "index":0,
+                "productId": "5ac21c46040ab15980c9b442",
+                "emojiId": "020"
+            },
+                        {
+                "index":7,
+                "productId": "5ac21c46040ab15980c9b442",
+                "emojiId": "020"
+            }
+        ]
+    
+    text_message = TextSendMessage(text = '''$ Tyler $ Hello~ 歡迎您的加入~成為Tyler的好友!
+    我是Tyler 您的小幫手
+    這裡有許多的股票資訊
+   可以直接點選下面的功能來使用~''', emojis = emoji)
+  
+  
+   # message = TextSendMessage(text = event.message.text)
+    #line_bot_api.reply_message(event.reply_token,message)
 
+    Sticker_message = StickerSendMessage(
+        package_id = '11539',
+        sticker_id='52114118'
+    )
+    line_bot_api.reply_message(
+        event.reply_token,
+        [text_message, Sticker_message])
 if __name__ == '__main__':
     app.run()
