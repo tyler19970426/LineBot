6cfd2f37dce2e1262b2fd66e0e5a1c8407c6a3f8
tyler19970426 <139113291+tyler19970426@users.noreply.github.com>
2023-08-07 14:24:16 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def callback():
 def handel_message(event):  
     message_text = str(event.message.text).lower()
 
-    if message_text == '@最新消息':
+    if message_text == '@使用說明':
         about_us_event(event)
         Usage(event)
 if __name__ == '__main__':
