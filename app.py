7d3bebcf54124cf0b3d0099497447d4b8e527861
tyler19970426 <139113291+tyler19970426@users.noreply.github.com>
2023-08-09 13:57:10 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -63,11 +63,14 @@ def handel_message(event):
 #新增使用者關注的股票到mongodb
     if re.match("關注[0-9]{4}[<>][0-9]",msg):#使用者新增股票至股票清單    
         stockNumber = msg[2:6]
+        line_bot_api.push_message(uid, TextSendMessage('加入股票代號'+stockNumber))
         content = write_my_stock(uid, user_name,stockNumber,msg[6:7],msg[7:])
-    else:
-        content = write_my_stock(uid, user_name, stockNumber, "未設定",'未設定')
         line_bot_api.push_message(uid, TextSendMessage(content))
         return 0
+    # else:
+    #     content = write_my_stock(uid, user_name, stockNumber, "未設定",'未設定')
+    #     line_bot_api.push_message(uid, TextSendMessage(content))
+    #     return 0
 
     if (emsg.startswith('#')):
         text = emsg[1:]
